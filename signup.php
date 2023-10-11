0b6dd46c4d1db2d8d537cbe248745ca2473e5405
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-10-11 19:50:26 -0400

    46 - signuppage branch added a popup that displays particular error upon unsuccessful signup before redirecting back to sign up page. will replace redirect files with actual frontend files when done.

--- signup.php
+++ signup.php
@@ -18,28 +18,47 @@ function generateID(){
     }
 }
 
+//This function creates a popup message with a custom message and then redirects to another page
+
+function popUp($message,$redirect){
+    echo '<script type="text/javascript">'; 
+    echo 'alert("'.$message.'");';
+    echo 'window.location.href = "'.$redirect.'";';
+    echo '</script>';
+}
+
 //This function quantifies the strength of the password, making sure the user includes 
 //lower case letters, upper case letters, numbers, and special characters. 
 
 function passwordStrength($password){
     if (strlen($password) < 8){
-        echo "password is too short || \n";
+        $message = "password is too short";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);    
         return false;
     }
     if (preg_match('/[A-Z]/', $password) == false){
-        echo "password does not contain upper case letters || \n";
+        $message = "password does not contain uppercase letters";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         return false;
     }
     if (preg_match('/[a-z]/', $password) == false){
-        echo "password does not contain lower case letters || \n";
+        $message = "password does not contain lowercase letters";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         return false;
     }
     if (preg_match('/[0-9]/', $password) == false){
-        echo "password does not contain numbers || \n";
+        $message = "password does not contain numbers";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         return false;
     }
     if (preg_match('/[!@#$%^&*()\-_=+{};:,<.>]/', $password) == false){
-        echo "password does not contain special characters || \n";
+        $message = "password does not contain special characters";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         return false;
     }
     return true;
@@ -53,8 +72,6 @@ function signUpSQL(){
     $username = getInfo("username");
     $password = getInfo("password");
     if (passwordStrength($password) == false){
-        //return false;
-        header("Location: testsignup.html");
         exit();
     }
     $hashed_password = hash("sha256",$password);
@@ -63,37 +80,37 @@ function signUpSQL(){
     $sql = "SELECT username FROM logins WHERE username = '$username'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0){
-        //echo "Username is taken || \n";
-        header("Location: testsignup.html");
-        //return false;
+        $message = "This username is already taken";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         exit();
     }
     if (strlen($username) == 0 || strlen($password) == 0 || strlen($email) == 0){
-        //echo "Please fill out all fields || \n";
-        header("Location: testsignup.html");
-        //return false;
+        $message = "Please fill out all fields";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         exit();
     }
     $sql = "SELECT email FROM logins WHERE email = '$email'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0){
-        //echo "An account has already been registered with this email || \n";
-        header("Location: testsignup.html");
-        //return false;
+        $message = "This email is already registered to another user";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);
         exit();
     }
     $randID = generateID();
     $sql = "INSERT INTO logins (username, password, email, account_id) VALUES ('$username', '$hashed_password', '$email', '$randID')";
     $result = $conn->query($sql);
     if ($result === TRUE) {
-        //echo "New user created successfully || \n";
-        header("Location: testlogin.html");
-        //return true;
+        $message = "New user created succsessfully";
+        $redirect = "testlogin.html";
+        popUp($message,$redirect);
         exit();
     } else {
-        //echo "Error: " . $sql . "<br>" . $conn->error . " || \n";
-        header("Location: testsignup.html");
-        //return false;
+        $message = "Unsuccsessful signup";
+        $redirect = "testsignup.html";
+        popUp($message,$redirect);    
         exit();
     }
 }
@@ -101,5 +118,6 @@ function signUpSQL(){
 
 $conn = connect();
 signUpSQL();
+
 
 ?>
