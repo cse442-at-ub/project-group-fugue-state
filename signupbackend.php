eeb3ce6632d7ba8c4128f8f8337430dfe09b19ab
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-05 13:24:22 -0500

    added extra slash after user id for directory name

--- signupbackend.php
+++ signupbackend.php
@@ -123,7 +123,7 @@ function signUpSQL(){
         $result = $conn->query($sql);
         if ($result === TRUE) {
              // Make folder path
-             $userPath = $usersPath . strval($randID);
+             $userPath = $usersPath . strval($randID) . "/";
              // Create folder for new user
              mkdir($userPath);
              $message = "New user created succsessfully";
