5dbf06a18aa63b457e3552c775209a312489952a
Lunamoon11 <erichong323@gmail.com>
2023-11-06 14:25:37 -0500

    #57 #58 set the search functionality + search page

---
+++ Frontend/templates/TaylorSwift.php
@@ -0,0 +1,75 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css"
+            href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/globals.css">
+        <link rel="stylesheet" type="text/css"
+            href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/styleguide.css">
+        <link rel="stylesheet" type="text/css"
+            href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/style.css">
+        <link rel="stylesheet" type="text/css"
+            href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/image.css">
+    </head>
+    <body>
+        <div class="desktop-home-page">
+            <div class="div">
+                <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/tbd.php" class='settings'>
+                    <div class="ellipse-wrapper">
+                        <div class="ellipse"></div>
+                    </div>
+                </a>
+                <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/profile.php" class='profile-icon'>
+                    <div class="overlap-4">
+                        <div class="ellipse-3"></div>
+                        <img class="img" src= "/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/ellipse-4-2.svg" />
+                    </div>
+                </a>
+                <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/homepage.php" class='logo-icon'>
+                    <img class="logo" src = "/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/logo.png" />
+                </a>
+                <div class = "first_song">
+                    <img class="first_pic" src="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/TaylorSwift/BS.png" />
+                </div>
+                <div class="first_song_des">
+                    <div class = "title">
+                        Blank Space
+                    </div>
+                    <div>
+                        Taylor Swift
+                    </div>
+                    <div>
+                        Produced by Shellback & Max Martin
+                    </div>
+                </div>
+                <div class = "second_song">
+                    <img class="second_pic" src="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/TaylorSwift/CS.png" />
+                </div>
+                <div class="second_song_des">
+                    <div class="title">
+                        Cruel Summer
+                    </div>
+                    <div>
+                        Taylor Swift
+                    </div>
+                    <div>
+                        Produced by Taylor Swift & Jack Antonoff
+                    </div>
+                </div>
+                <div class = "third_song">
+                    <img class="third_pic" src="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/TaylorSwift/YBWM.png" />
+                </div>
+                <div class="third_song_des">
+                    <div class="title">
+                        You Belong With Me
+                    </div>
+                    <div>
+                        Taylor Swift
+                    </div>
+                    <div>
+                        Produced by Nathan Chapman & Taylor Swift
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
