13f35c723bb13325bc1594a6acde1d91d3cadee6
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-06 19:34:42 -0500

    added code in logoutbackend that unsets variables beforesdestryoing session

--- logoutbackend.php
+++ logoutbackend.php
@@ -5,6 +5,7 @@ require "connect.php";
 if (getInfo("logout") == "true"){
     global $homePath;
     session_start();
+    session_unset();
     session_destroy();
     $message = "Logout successful";
     popUp($message);
