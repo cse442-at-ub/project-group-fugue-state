0489f06096a56d87e5792bc9f7bfa6970743225f
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-07 19:22:43 -0500

    changed from sending aform to vreatinga  session vairable

--- Frontend/templates/profile.php
+++ Frontend/templates/profile.php
@@ -37,7 +37,7 @@ if (isset($_SESSION["logged_in"])){
                 
                 <a href="<?php echo $_SESSION['redirect']; ?>" class='sign-in'>
                     <div class="text-wrapper-2">
-                        <?php echo $_SESSION["button"]; ?>
+                        <?php $_SESSION["logged_out"] = true; echo $_SESSION["button"]; ?>
                         <form action=<?php echo $_SESSION["redirect2"]; ?> method="post">
                             <!--<input class="text-wrapper-2" type="submit" id="true" name = "true" value=/></a>-->
                             <input class="text-wrapper-2" type="hidden" id="true" name="true" value="true">
