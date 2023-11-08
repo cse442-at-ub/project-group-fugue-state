8003f918231c95b5d9e8da5009db6f7da0ce720a
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-07 19:20:03 -0500

    button redirects to correct pages but on signout wont invoke php sign out script, trying something

--- Frontend/templates/profile.php
+++ Frontend/templates/profile.php
@@ -40,7 +40,7 @@ if (isset($_SESSION["logged_in"])){
                         <?php echo $_SESSION["button"]; ?>
                         <form action=<?php echo $_SESSION["redirect2"]; ?> method="post">
                             <!--<input class="text-wrapper-2" type="submit" id="true" name = "true" value=/></a>-->
-                            <input class="text-wrapper-2" type="hidden" id="true" name="logout" value="true">
+                            <input class="text-wrapper-2" type="hidden" id="true" name="true" value="true">
                         </form>
                     </div>
                 </a>   
