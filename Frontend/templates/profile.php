742fda729196f70b21e6e612c3d16fad52d40764
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-07 19:04:29 -0500

    i think this will work to get button to properly display in profile.php. removed a div

--- Frontend/templates/profile.php
+++ Frontend/templates/profile.php
@@ -37,7 +37,7 @@ if (isset($_SESSION["logged_in"])){
                     <div class="text-wrapper-2">
                         <?php echo $_SESSION["button"]; ?>
                         <form action=<?php echo $_SESSION["redirect"]; ?> method="post">
-                            <input class="text-wrapper-2" type="submit" id="true" name = "true" value=<?php echo $_SESSION["button"]; ?> />
+                            <input class="text-wrapper-2" type="submit" id="true" name = "true"/>
                             <input class="text-wrapper-2" type="hidden" name="logout" value="true">
                         </form>
                     </div>
