17990525222435690ea8f3acec104f311281d873
js642 <js642@buffalo.edu>
2023-11-09 12:31:32 -0500

    added functions for getting pages, arrangement, chord progression, and lyrics

--- readSong.php
+++ readSong.php
@@ -33,5 +33,50 @@ function getKey(int $songID){
     return "";
 }  
 
+// This function will read a song id and return the number of pages
+function getPages(int $songID){
+    global $conn;
+    $sql = "SELECT pages FROM songs WHERE song_id = '$songID'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        return $result->fetch_assoc()["pages"];
+    }
+    return "";
+}
+
+// This function will read a song id and return the arrangment as an array of objects
+function getArrangement(int $songID){
+    global $conn;
+    $sql = "SELECT arrangement FROM songs WHERE song_id = '$songID'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        return $result->fetch_assoc()["arrangement"];
+    }  
+    return "";
+}
+
+// This function will read a song id and return the chord progressions as an array of objects
+function getChords(int $songID){
+    global $conn;
+    $sql = "SELECT chord_progression FROM songs WHERE song_id = '$songID'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        return $result->fetch_assoc()["chord_progression"];
+    }  
+    return "";
+}
+
+
+// This function will read a song id and return the lyrics as an array of objects
+function getLyrics(int $songID){
+    global $conn;
+    $sql = "SELECT lyrics FROM songs WHERE song_id = '$songID'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0){
+        return $result->fetch_assoc()["lyrics"];
+    }  
+    return "";
+}
+
 $conn = connect();
 ?>
