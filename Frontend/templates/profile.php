f4a079be66d1bfdfb994dcbfe68a3e5ed5ea07c6
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-11-09 12:21:30 -0500

    added code in profile.php that turns the sign in or out link into a button that sends a form

--- Frontend/templates/profile.php
+++ Frontend/templates/profile.php
@@ -40,13 +40,22 @@ if (isset($_SESSION["logged_in"])){
                 <!-- <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/homepage.php" class='sign-in'>
                     <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
                 </a> -->
+                <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/login.php" id="inoroutlink" class='sign-in'>
+                    <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
+                </a> 
 
-                <div class="sign-in">
-                    <form action=<?php echo $_SESSION["redirect2"]; ?> method="post">
-                        <input class="text-wrapper-2" type="submit" id="true" name = "true" value=<?php echo $_SESSION["button"]; ?> />
-                        <input class="text-wrapper-2" type="hidden" name="logout" value="true">
-                    </form>
-                </div>
+                <form action=<?php echo $_SESSION["redirect2"]; ?> method="post" id="inoroutform">
+                    <input class="text-wrapper-2" type="hidden" name="logout" value="true">
+                </form>
+
+                <script>
+                    function submitForm(){
+                        document.getElementById("inoroutform").submit();
+                    }
+
+                    document.getElementById("inoroutlink").addEventListener("click", submitForm());
+
+                </script>
 
 
                 <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/tbd.php" class='settings'>
