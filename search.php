923de852ceeaf671375b416c748a28d0937e4a56
cronrad <conradbr@buffalo.edu>
2023-11-10 18:04:17 -0500

    # 61 Testing recently searched

--- search.php
+++ search.php
@@ -9,6 +9,41 @@ if (isset($_GET['q'])) {
 
     $checkartist = "SELECT * FROM songs WHERE LOWER(songwriter) = '$search_query'";
     $resultartist = $conn->query($checkartist);
+    # DEBUG START ---
+    // Add page to user's recent searches list
+    $account_id = $_SESSION['account_id'];
+    $song = $search_query;
+    // recentSearches($account_id, $songname); -------
+    $checkSql = "SELECT COUNT(*) as count FROM recent_songs WHERE account_id = $account_id";
+    $resultCheck = $conn->query($checkSql);
+    $row = $resultCheck->fetch_assoc();
+    $exists = $row['count'];
+
+    if ($exists > 0) {
+        // If the account_id exists, update the recent songs
+        $updateSql = "SELECT song_1, song_2, song_3 FROM recent_songs WHERE account_id = $account_id";
+        $result = $conn->query($updateSql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if ($row["song_1"] == "None") {
+                $sql = "UPDATE recent_songs SET song_1 = '$song' WHERE account_id = $account_id";
+            } elseif (($row["song_2"] == "None")) {
+                $sql = "UPDATE recent_songs SET song_2 = '$song' WHERE account_id = $account_id";
+            } elseif (($row["song_3"]) == "None") {
+                $sql = "UPDATE recent_songs SET song_3 = '$song' WHERE account_id = $account_id";
+            } else {
+                $sql = "UPDATE recent_songs SET song_1 = '$song' WHERE account_id = $account_id";
+            }
+            $conn->query($sql);
+        }
+    } else {
+        // If the account_id does not exist, insert a new record
+        $insertSql = "INSERT INTO recent_songs (account_id, song_1, song_2, song_3) 
+                      VALUES ('$account_id', '$song', 'None', 'None')";
+        $conn->query($insertSql);
+    }
+    # DEBUG END ---
     
     if ($resultartist->num_rows > 0) {
 
