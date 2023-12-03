bc2777dea1bec8ace06102baa38b90a155fe077d
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2023-12-03 16:15:12 -0500

    fixed the error it was a typo in a variable name

--- forgotpwdbackend.php
+++ forgotpwdbackend.php
@@ -19,19 +19,19 @@ function resetPassword(){
     $newhashedpassword = hash("sha256",$newpassword);
     $sql = "SELECT code FROM resetCodes WHERE username = '$username'";
     $result = $conn->query($sql);
-    //if ($result->num_rows == 0){
-    //    popUp("No reset code is registered with this email or username");
-    //    redirectPage($forgotPath);
-    //}
+    if ($result->num_rows == 0){
+        popUp("No reset code is registered with this email or username");
+        redirectPage($forgotPath);
+    }
     $result = $result->fetch_assoc()["code"];
 
-    //$sql2 = "SELECT password FROM logins WHERE username = '$username'";
-    //$result2 = $conn->query($sql2);
-    //$result2 = $result->fetch_assoc()["password"];
-    //if ($result2 == $newhashedpassword){
-    //    popUp("New password cannot be the same as the old password");
-    //    redirectPage($forgotPath);
-    //}
+    $sql2 = "SELECT password FROM logins WHERE username = '$username'";
+    $result2 = $conn->query($sql2);
+    $result2 = $result2->fetch_assoc()["password"];
+    if ($result2 == $newhashedpassword){
+        popUp("New password cannot be the same as the old password");
+        redirectPage($forgotPath);
+    }
     if ($result == $code){
         $sql = "UPDATE logins SET password = '$newhashedpassword' WHERE username = '$username'";
         $conn->query($sql);
