2d7302c08633ee58c833105aa0ca23fb9233d990
cjlehr <cjlehr@buffalo.edu>
2023-12-04 01:18:41 -0500

    #65 created new look for song creation page

--- Frontend/templates/songeditor.php
+++ Frontend/templates/songeditor.php
@@ -16,139 +16,186 @@
         <div class="overlap">
 
         </div>
+        <form action="#" method="post">
+    <label for="Title">Title:</label>
+    <input type="text" name="formTitle" id="formTitle" required />
 
-        <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/homepage.php" class='logo-icon'>
-          <img class="logo" src = "/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/logo.png" />
-        </a>
-        <!--<img class="logo" src="img/logo.png" />-->
-
-          <style>
-            div.relative {
-              position: relative;
-              top:100px;
-              left: 400px;
-            }
-            h1 {
-                font-size: 32px;
-            }
-            
-
-          </style>
-          <div class="relative">
-            <h1>TITLE</h1>
-            <form action="editorbackend.php" method="post">
-              <input type="input" id="title" name="title" placeholder="Title" value=""><br><br>
-              <a href="">
-                <input type="submit" value="submit">
-              </a>
-          <div>
-          <style>
-            h2{
-              position: relative;
-              top: 450px;
-              left: 100px;
-            }
-            div.section{
-              position: relative;
-              top: 450px;
-              left: 0px;
-            }
-            div.key{
-              position: relative;
-              top: 10px;
-              left: 0px;
-            }
-            div.lineChords{
-              position: relative;
-              top: 10px;
-              left: 0px;
-            }
-            div.lyrics{
-              position: relative;
-              top: 10px;
-              left: 0px;
-
-              width: 50%;
-            }
-            
-          </style>
-          <div class="lineEditor">
-            </form>
-            <h2>line editor</h2>
-            <form action="editorbackend.php" method="post">
-            	<div class = "section">
-                <label for="section">Section:</label>
-                
-                <select id="section" name="section">
-                  <option value="chorus">chorus</option>
-                  <option value="verse">verse</option>
-                  <option value="bridge">bridge</option>
+    <table>
+        <tr>
+            <td>
+                <label for="line1"> line:</label>
+            </td>
+            <td>
+                <select name="line1_section_dropdown" id="line1">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                <div>
-                <div class = "key">
-                <label for="key">Key:</label>
-                <select id="key" name="key">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line1_text" id="line1_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line1_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line1_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        
+        
+        <td>
+                <label for="line2"> line:</label>
+            </td>
+            <td>
+                <select name="line2_section_dropdown" id="line2">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                
-                 <select id="mm" name="mm">
-                  <option value="Major">Major</option>
-                  <option value="Minor">Minor</option>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line2_text" id="line2_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line2_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line2_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="line3"> line:</label>
+            </td>
+            <td>
+                <select name="line3_section_dropdown" id="line3">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                <div>
-                <div class = "lineChords">
-                <label for="chord">chord:</label>
-                <select id="col1" name="col1">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line3_text" id="line3_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line3_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line3_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        
+        
+        <td>
+                <label for="line4"> line:</label>
+            </td>
+            <td>
+                <select name="line4_section_dropdown" id="line4">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                <select id="col2" name="col2">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line4_text" id="line4_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line4_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line4_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for="line5"> line:</label>
+            </td>
+            <td>
+                <select name="line5_section_dropdown" id="line5">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                <select id="col3" name="col3">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line5_text" id="line5_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line5_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line5_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        
+        
+        <td>
+                <label for="line6"> line:</label>
+            </td>
+            <td>
+                <select name="line2_section_dropdown" id="line6">
+                    <option value="nothing"> </option>
+                    <option value="intro">intro</option>
+                    <option value="chorus">chorus</option>
+                    <option value="verse">verse</option>
+                    <option value="bridge">bridge</option>
                 </select>
-                <select id="col4" name="col4">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col5" name="col5">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col6" name="col6">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col7" name="col7">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col8" name="col8">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col9" name="col9">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <select id="col10" name="col10">
-                  <option value="A">A</option>
-                  <option value="C">C</option>
-                </select>
-                <div>
-                <div class = "lyrics">
-                <input type="input" id="lyrics" name="lyrics" placeholder="lyrics" value=""><br>
-                <div>
-               <a href="">
-                <input type="submit" value="submit">
-               </a>
-            </form>
-
-            </a>
+            </td>
+            <td>
+               
+            </td>
+            <td>
+                <input type="text" name="line6_text" id="line6_text" />
+            </td>
+            <td>
+                <label>
+                    <input type="radio" name="line6_option" value="chord" /> Chord
+                </label>
+                <label>
+                    <input type="radio" name="line6_option" value="lyric" /> Lyric
+                </label>
+            </td>
+        </tr>
+        
+    </table>
+    <input type="submit" value="Create new Page">
+    <input type="submit" value="Submit">
       </div>
     </div>
   </body>
