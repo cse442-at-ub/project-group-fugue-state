6ecb3429a502762126880623ef87501f470f1172
cjlehr <cjlehr@buffalo.edu>
2023-12-07 15:42:16 -0500

    #65 changed positioning of the editor

--- Frontend/templates/songeditor.php
+++ Frontend/templates/songeditor.php
@@ -23,45 +23,54 @@
             <a href="/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/templates/homepage.php" class='logo-icon'>
             <img class="logo" src = "/CSE442-542/2023-Fall/cse-442o/project-group-fugue-state/Frontend/static/img/logo.png" />
             </a>
-            <form action="#" method="post">
-                <label for="Title">Title:</label>
-                <input type="text" name="formTitle" id="formTitle" required />
-                <label for="Key">Title:</label>
-                <input type="text" name="key" id="key" required />
-                <table id="songTable">
-                    <!-- Row template for cloning -->
-                    <tr class="hidden-row" id="rowTemplate">
-                        <td>
-                            <label for="line">Line:</label>
-                        </td>
-                        <td>
-                            <select name="section_dropdown" class="section-dropdown">
-                                <option value="nothing"> </option>
-                                <option value="intro">intro</option>
-                                <option value="chorus">chorus</option>
-                                <option value="verse">verse</option>
-                                <option value="bridge">bridge</option>
-                            </select>
-                        </td>
-                        <td></td>
-                        <td>
-                            <input type="text" name="line_text" class="line-text" />
-                        </td>
-                        <td>
-                            <label>
-                                <input type="radio" name="line_option" value="chord" /> Chord
-                            </label>
-                            <label>
-                                <input type="radio" name="line_option" value="lyric" /> Lyric
-                            </label>
-                        </td>
-                    </tr>
-                </table>
-                
-                <button type="button" onclick="addRows(10)">Add page</button>
-                <input type="submit" value="Submit">
-            </form>
+            <style>
+                 div.editor {
+                    position: relative;
+                    top:260px;
+                    left: 400px;
+                    }
+            </style>
 
+            <div class="editor">
+                <form action="#" method="post">
+                    <label for="Title">Title:</label>
+                    <input type="text" name="formTitle" id="formTitle" required />
+                    <label for="Key">Title:</label>
+                    <input type="text" name="key" id="key" required />
+                    <table id="songTable">
+                        <!-- Row template for cloning -->
+                        <tr class="hidden-row" id="rowTemplate">
+                            <td>
+                                <label for="line">Line:</label>
+                            </td>
+                            <td>
+                                <select name="section_dropdown" class="section-dropdown">
+                                    <option value="nothing"> </option>
+                                    <option value="intro">intro</option>
+                                    <option value="chorus">chorus</option>
+                                    <option value="verse">verse</option>
+                                    <option value="bridge">bridge</option>
+                                </select>
+                            </td>
+                            <td></td>
+                            <td>
+                                <input type="text" name="line_text" class="line-text" />
+                            </td>
+                            <td>
+                                <label>
+                                    <input type="radio" name="line_option" value="chord" /> Chord
+                                </label>
+                                <label>
+                                    <input type="radio" name="line_option" value="lyric" /> Lyric
+                                </label>
+                            </td>
+                        </tr>
+                    </table>
+                    
+                    <button type="button" onclick="addRows(10)">Add page</button>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
             <script>
                 function addRows(numRows) {
                     var table = document.getElementById("songTable");
