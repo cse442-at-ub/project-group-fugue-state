cbeb8c0eb78cd39ca673c4d2da7cabaab552d78c
Lunamoon11 <erichong323@gmail.com>
2023-12-08 12:41:17 -0500

    #78 title and songwriter search

--- search.php
+++ search.php
@@ -43,12 +43,12 @@ if (isset($_GET['q'])) {
 
     if (isset($_GET['searchType'])){
         $searchType = $_GET['searchType'];
-        if ($searchType == 'artist'){
+        if ($searchType == 'songwriter'){
             $checkartist = "SELECT * FROM songs WHERE songwriter = '$search_query'";
             $resultartist = $conn->query($checkartist);
             
             if ($resultartist->num_rows > 0) {
-                $artistpage = '/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/artist_song_page.php?artist=' . urlencode($search_query) . '&searchType=artist';
+                $artistpage = '/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/artist_song_page.php?artist=' . urlencode($search_query) . '&searchType=songwriter';
                 if (isset($_SESSION["logged_in"]) == true){
                     $account_id = $_SESSION['account_id'];
                     $song = $search_query;
@@ -104,46 +104,7 @@ if (isset($_GET['q'])) {
         }
     }
     
-    $checkartist = "SELECT * FROM songs WHERE songwriter = '$search_query'";
-    $resultartist = $conn->query($checkartist);
     
-    if ($resultartist->num_rows > 0) {
-
-        $artistpage = '/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/artist_song_page.php?artist=' . urlencode($search_query);
-        if (isset($_SESSION["logged_in"]) == true){
-            $account_id = $_SESSION['account_id'];
-            $song = $search_query;
-            recentlySearched($conn, $account_id, $song);
-        }
-        
-        // Redirect to the artist's page
-        header("Location: $artistpage");
-        exit();
-    }
-        
-    $checktitle = "SELECT * FROM songs WHERE LOWER(title) = '$search_query'";
-    $resulttitle = $conn->query($checktitle);
-
-    
-    if ($resulttitle->num_rows > 0) {
-
-        $row = $resulttitle->fetch_assoc();
-        $song_id = $row['song_id'];
-        $_SESSION['current_song_id'] = $song_id;
-        
-        if (isset($_SESSION["logged_in"]) == true){
-            $account_id = $_SESSION['account_id'];
-            $song = $search_query;
-            recentlySearched($conn, $account_id, $song);
-        }
-        
-        // Redirect to the songview's page
-        $songViewPage = '/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/songView.php?song_id=' . $song_id;
-        header("Location: $songViewPage");
-        exit();
-    }
-    
-
     } else {
         $homepage = '/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/homepage.php';
         header("Location: $homepage");
