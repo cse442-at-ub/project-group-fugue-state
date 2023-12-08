c8e1649b5460c8b57550a01575225c666fa11306
Lunamoon11 <erichong323@gmail.com>
2023-12-08 01:01:46 -0500

    #74 homepage

--- Frontend/templates/homepage.php
+++ Frontend/templates/homepage.php
@@ -39,7 +39,36 @@ else{
     <link rel="stylesheet" type="text/css" href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/profile.css"> 
   </head>
   <body>
-    <p>Logged in As: <?php echo $_SESSION["username"]; ?> </p>
+    <div class="top">
+      <div class="yo">
+        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/homepage.php" class="logo-icon">
+          <img class="logo" src="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/img/logo.png">
+        </a>
+      </div>
+      <div class="to">
+        <form action="../../search.php" method="get" id="search-bar" data-gtm-form-interact-id="0"> 
+          <input type="search" class="search-bar" id="query" name="q" placeholder="Search..." aria-label="Search through site content" data-gtm-form-interact-field-id="0">
+        
+          <button type="submit" class="search-button">Search</button>
+        </form>
+      </div>
+      <div class="uo">
+        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/login.php" id="inoroutlink" class='sign-in'>
+          <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
+        </a>
+        <a href="<?php echo $_SESSION["redirect3"]; ?>" class='profile-icon'>
+          <div class="overlap-4">
+            <div class="ellipse-3"></div>
+            <img class="img" src= "/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/img/ellipse-4-2.svg" />              
+          </div>
+        </a>
+        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/tbd.php" class="settings">
+          <div class="ellipse-wrapper">
+            <div class="ellipse"></div>
+          </div>
+        </a>
+    </div>
+    </div>
     <div class="desktop-home-page">
       <div class="div">
         <div class="overlap">
@@ -119,20 +148,11 @@ else{
               <div class="text-3">Progress</div>
             </div>
           </a>                  
-        </div>
-        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/login.php" id="inoroutlink" class='sign-in'>
-          <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
-        </a> 
+        </div> 
 
         <form action=<?php echo $_SESSION["redirect2"]; ?> method="post" id="inoroutform">
           <input class="text-wrapper-2" type="hidden" name="logout" value="true">
         </form>    
-
-        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/tbd.php" class='settings'>
-          <div class="ellipse-wrapper">
-            <div class="ellipse"></div>
-          </div>
-        </a>
         <style>
           form {
             position: relative;
@@ -171,17 +191,14 @@ else{
           }
 
           radio {
-            position: relative
+            position: relative;
             top: 25px;
             width: 5px;
             height: 5px;
           }
           
         </style>
-        <form action = "../../search.php" method = "get"   id= "search-bar"> 
-          <input type="search" id="query" name="q" 
-          placeholder="Search..." 
-          aria-label="Search through site content">
+        <!--
           <div>
             <label>
               <input type="radio" name="searchType" value="genres" checked>
@@ -198,19 +215,8 @@ else{
               Title
             </label>
           </div>
-      
-          <button type="submit">Search</button>
-        </form>
-        <a href="<?php echo $_SESSION["redirect3"]; ?>" class='profile-icon'>
-          <div class="overlap-4">
-            <div class="ellipse-3"></div>
-            <img class="img" src= "/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/img/ellipse-4-2.svg" />              
-          </div>
-        </a>        
-        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/homepage.php" class='logo-icon'>
-          <img class="logo" src = "/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/img/logo.png" />
-        </a>        
-        <div class="rectangle-4"></div>
+        -->
+                
       </div>
       <div class="recent-searches-tab-homepage">
                     <div class="font">Recent Songs</div>
