3161fd7689e07b5e7a78f71b6789734a87b7475f
Lunamoon11 <erichong323@gmail.com>
2023-12-08 02:18:30 -0500

    #74 profile icon and sign in

--- Frontend/templates/homepage.php
+++ Frontend/templates/homepage.php
@@ -48,19 +48,18 @@ else{
       <div class="to">
         <form action="../../search.php" method="get" id="search-bar" data-gtm-form-interact-id="0"> 
           <input type="search" class="search-bar" id="query" name="q" placeholder="Search..." aria-label="Search through site content" data-gtm-form-interact-field-id="0">
-        
           <button type="submit" class="search-button">Search</button>
         </form>
       </div>
       <div class="uo">
-        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/login.php" id="inoroutlink" class='sign-in'>
-          <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
-        </a>
-        <a href="<?php echo $_SESSION["redirect3"]; ?>" class='profile-icon'>
+      <a href="<?php echo $_SESSION["redirect3"]; ?>" class='profile-icon'>
           <div class="overlap-4">
             <div class="ellipse-3"></div>
             <img class="img" src= "/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/static/img/ellipse-4-2.svg" />              
           </div>
+        </a>
+        <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/login.php" id="inoroutlink" class='sign-in'>
+          <div class="text-wrapper-2"><?php echo $_SESSION["button"]; ?></div>
         </a>
         <a href="/CSE442-542/2023-Fall/cse-442o/git_repo/project-group-fugue-state/Frontend/templates/tbd.php" class="settings">
           <div class="ellipse-wrapper">
